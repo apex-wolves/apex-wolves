09ea385caa253e166860637acfc55be8f6e4accc
apex-wolves <mr.alan.tran@gmail.com>
2022-02-22 21:59:46 -0800

    just meh

--- src/views/transmission.jsx
+++ src/views/transmission.jsx
@@ -5,7 +5,7 @@ class Transmission extends React.Component {
 
   render() {
     return (
-      <main>
+      <main className="aw-transmission-bg">
         <div className="video-container"><iframe className="video" src="https://streamable.com/e/9h36sw?loop=0" frameborder="0" width="100%" height="100%" allowfullscreen></iframe></div>
       </main>
         
