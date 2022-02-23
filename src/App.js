4c756d00eaa2e2cbc96cdc3df9bbac74e124be66
apex-wolves <mr.alan.tran@gmail.com>
2022-02-22 20:42:05 -0800

    transmission

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.scss";
 import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "./views/home";
 import GPS from "./views/GPS";
+import Transmission from "./views/transmission";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/GPS" element={<GPS />} />
+      <Route path="/TRANSMISSION" element={<Transmission />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
---
+++ src/views/transmission.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './transmission.scss';
+
+class Transmission extends React.Component {
+
+  componentDidMount(){
+    document.querySelectorAll(".ytp-endscreen-content").forEach((el) => el.remove());
+  }
+
+  render() {
+    return (
+      <main>
+        <div className="video-container"><iframe className="video" src="https://streamable.com/e/9h36sw?loop=0" frameborder="0" width="100%" height="100%" allowfullscreen></iframe></div>
+      </main>
+        
+    );
+  }
+}
+
+export default Transmission;
